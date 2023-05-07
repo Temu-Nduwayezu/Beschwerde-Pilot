bb635886fe01ddea8a6f744b970a24eecdb7c5f5
Hamidemutlu <71645913+Hamidemutlu@users.noreply.github.com>
2023-05-07 14:31:10 +0200

    text daten dazugetan we fucked up

--- index.js
+++ index.js
@@ -10,24 +10,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/login.html');
-  console.log("root")
   //res.sendFile('/public/login.html')
 });
 
 app.post('/create-pdf', async (req, res) => {
+  const behoerde = req.body.behoerde;
+  const adresse = req.body.adresse;
+  const ort = req.body.ort;
+  const datum = req.body.datum;
   const name = req.body.name;
+  const adresse1 = req.body.adresse1;
   const email = req.body.email;
+  const saumnisbehorde = req.body.saumnisbehorde;
+
 
   // Create a new PDF document
   const pdfDoc = await PDFDocument.create();
+  const paragraph = "Gegen die mehr als 6-monatige Untätigkeit (belangte Behörde) bei der Erledigung \n eines Antrages auf (Antragsgegenstand) vom (Datum), erhebe ich wegen \n Verletzung in meinem Recht auf Entscheidung"
 
   // Add a new page to the document
   const page = pdfDoc.addPage();
 
   // Draw the text on the page
   const { width, height } = page.getSize();
-  page.drawText(`Name: ${name}`, { x: 50, y: height - 50 });
-  page.drawText(`Email: ${email}`, { x: 50, y: height - 70 });
+  page.drawText(`Behörde: ${behoerde}`, { x: 50, y: height - 70 });
+  page.drawText(`Adresse: ${adresse}`, { x: 50, y: height - 90 });
+  page.drawText(`Ort: ${ort}`, { x: 50, y: height - 110 });
+  page.drawText(`Datum: ${datum}`, { x: 50, y: height - 130 });
+  page.drawText(`Name: ${name}`, { x: 50, y: height - 150 });
+  page.drawText(`Adresse: ${adresse}`, { x: 50, y: height - 170 });
+  page.drawText(`Email: ${email}`, { x: 50, y: height - 190 });
+  page.drawText(`Säumnisbeschwerde:`, { x: 50, y: height - 210 });
+  page.drawText(paragraph, { x: 50, y: 600, size: 10});
+
+  
 
   // Serialize the PDF document to a buffer
   const pdfBytes = await pdfDoc.save();
