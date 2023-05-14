a4d2226cd47792cab20e7f133e42ccc89ac97a12
MohamadArastu <mohamad.arasto@yahoo.com>
2023-05-14 12:02:57 +0200

    Finished Project, PDF works, New Design was impkemented

--- index.js
+++ index.js
@@ -20,73 +20,119 @@ app.use(bodyParser.urlencoded({
 //app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/create-pdf', async (req, res) => {
   const behoerde = req.body.behoerde;
   const adresse = req.body.adresse;
+
   const ort = req.body.ort;
   const datum = req.body.datum;
+
   const name = req.body.name;
   const adresse1 = req.body.adresse1;
-  const email = req.body.email;
+  const grund = req.body.grund;
+
+  //const email = req.body.email;
   const saumnisbehorde = req.body.saumnisbehorde;
 
-  
-  
+
   // Create a new PDF document
   const pdfDoc = await PDFDocument.create();
   const boldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
-  const paragraph = `Gegen die mehr als 6-monatige Untätigkeit ${behoerde} bei der Erledigung \n eines Antrages auf (Antragsgegenstand) vom ${datum}, erhebe ich wegen \n Verletzung in meinem Recht auf Entscheidung`
-  const beschwerde = `Beschwerde \n und stelle den/stelle durch meine bevollmächtigte Vertreterin den`
-  const antrag = `das Verwaltungsgericht möge über meinen Antrag auf Erteilung der Bewilligung zur (Antragsgegenstand)\n selbst in der Sache erkennen und mir die Bewilligung zur (Antragsgegenstand) erteilen,\n [in eventu] gem §24 VwGVG eine öffentliche Verhandlung durchführen.`
-  const begruenung = `Begründung Der Sachverhalt stellt sich wie folgt dar:\n Am ${datum} beantragte ich bei ${behoerde} die Erteilung der Bewilligung zur (Antragsgegenstand).`
+
+  const paragraph = `Gegen die mehr als 6-monatige Untätigkeit ${behoerde} bei der Erledigung eines Antrages auf\n ${grund} vom ${datum}, erhebe ich wegen Verletzung in meinem Recht auf Entscheidung`
+  const beschwerde = `und stelle den/stelle durch meine bevollmächtigte Vertreterin den`
+  const antrag = `das Verwaltungsgericht möge über meinen Antrag auf Erteilung der Bewilligung zur ${grund}\n selbst in der Sache erkennen und mir die Bewilligung zur ${grund} erteilen,\n [in eventu] gem §24 VwGVG eine öffentliche Verhandlung durchführen.`
+  const begruenung = `Begründung Der Sachverhalt stellt sich wie folgt dar:\n Am ${datum} beantragte ich bei ${behoerde} die Erteilung der Bewilligung zur ${grund}.`
   // Add a new page to the document
   const page = pdfDoc.addPage();
 
   // Draw the text on the page
-  const {width,height} = page.getSize();
-  page.drawText(`Behörde: ${behoerde}`, {
-    size:16,
+  const { width, height } = page.getSize();
+  page.drawText(`An die ${behoerde}`, {
+    size: 12,
     x: 50,
     y: height - 70
   });
-  page.drawText(`Adresse: ${adresse}`, {
-    size:16,
+  page.drawText(`${adresse}`, {
+    size: 12,
     x: 50,
     y: height - 90
   });
-  page.drawText(`${ort},${datum}`, {
-    size:16,
+  page.drawText(`${ort}, ${datum}`, {
+    size: 12,
     x: 450,
     y: height - 110
   });
   page.drawText(`Beschwerdeführer: ${name}`, {
-    size:16,
+    size: 12,
     x: 50,
     y: height - 150
   });
-  page.drawText(`Adresse: ${adresse}`, {
-    size:16,
+  page.drawText(`${adresse1}`, {
+    size: 12,
     x: 50,
     y: height - 170
   });
-  page.drawText(`Säumnisbeschwerde:`, {
-    size:16,
+  page.drawText(`Säumnisbeschwerde`, {
+    size: 14,
     x: 220,
     y: height - 210,
     font: boldFont
   });
-  page.drawText(paragraph, {
-    x: 50,
-    y: 600,
-    size: 10
+
+  page.drawText(`gem Art 130 Abs 1 Z 3 B-VG`, {
+    size: 12,
+    x: 220,
+    y: height - 230
   });
 
-  page.drawText(beschwerde, {x: 50, y: 500, size: 10});
-  page.drawText(antrag, {x: 50, y: 400, size: 10});
-  page.drawText(begruenung, {x: 50, y: 300, size: 10});
+  page.drawText(paragraph, {
+    x: 50,
+    y: height - 270,
+    size: 12
+  });
+
+  page.drawText(`Beschwerde`, {
+    x: 250,
+    y: height - 340,
+    size: 14,
+    font: boldFont
+  });
+
+  page.drawText(beschwerde, {
+    x: 50,
+    y: height - 370,
+    size: 12
+  });
+
+  page.drawText(`Antrag,`, {
+    x: 265,
+    y: height - 400,
+    size: 14,
+    font: boldFont
+  });
+
+  page.drawText(antrag, {
+    x: 50,
+    y: height - 440,
+    size: 12
+  });
+
+  page.drawText(`Begründung`, {
+    x: 250,
+    y: height - 530,
+    size: 14,
+    font: boldFont
+  });
+
+  page.drawText(begruenung, {
+    x: 50,
+    y: height - 570,
+    size: 12
+  });
 
   // Serialize the PDF document to a buffer
   const pdfBytes = await pdfDoc.save();
@@ -99,11 +145,11 @@ app.post('/create-pdf', async (req, res) => {
       res.send('Error generating PDF');
     } else {
       res.download('output.pdf');
-      if(IsCompleted){
+      if (IsCompleted) {
         cron.schedule('*/2 * * * *', () => {
           console.log('running a task every 5 minutes');
         });
-    }
+      }
     }
   });
 });
